b27e1396bffea901329aae4a009d39c6730543e6
Ulathi <swiftly.graphics@gmail.com>
2022-08-13 23:08:36 +0200

    compressed files

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
-const heroBtn = document.querySelector('.hero-cta');
-const cards = document.querySelectorAll('.cta-section__card');
-const dropdown = document.querySelector('.dropdown');
-const cardParent = document.querySelector('.cta-section__list');
-const navButton = document.querySelector('.hamburger-btn');
-const navModal = document.querySelector('.modal-nav');
-const listItems = document.querySelectorAll('.dropdown__item');
-
-const listItemsArr = Array.from(listItems);
-const cardsArr = Array.from(cards);
-
-// card hover basic effect
-
-cardParent.addEventListener('mouseover', event => {
-  if (cardsArr.includes(event.target)) {
-    const targetHTML = event.target;
-
-    cardsArr.forEach(cardEl => cardEl.classList.remove('card-active'));
-    targetHTML.classList.add('card-active');
-  } else return;
-});
-
-// hover functions
+const heroBtn = document.querySelector('.hero-cta'),
+  cards = document.querySelectorAll('.cta-section__card'),
+  dropdown = document.querySelector('.dropdown'),
+  cardParent = document.querySelector('.cta-section__list'),
+  navButton = document.querySelector('.hamburger-btn'),
+  navModal = document.querySelector('.modal-nav'),
+  listItems = document.querySelectorAll('.dropdown__item'),
+  listItemsArr = Array.from(listItems),
+  cardsArr = Array.from(cards);
 function onHamburgerHoverIn() {
   dropdown.classList.remove('closed');
 }
 function onHamburgerHoverOut() {
   dropdown.classList.add('closed');
 }
-
-// event listeners
-navButton.addEventListener('mouseover', onHamburgerHoverIn);
-navButton.addEventListener('mouseout', onHamburgerHoverOut);
-
-navButton.addEventListener('click', event => {
-  navModal.classList.remove('hidden');
-});
-
-navModal.addEventListener('click', () => {
-  navModal.classList.add('hidden');
-});
+cardParent.addEventListener('mouseover', e => {
+  if (cardsArr.includes(e.target)) {
+    const r = e.target;
+    cardsArr.forEach(e => e.classList.remove('card-active')),
+      r.classList.add('card-active');
+  }
+}),
+  navButton.addEventListener('mouseover', onHamburgerHoverIn),
+  navButton.addEventListener('mouseout', onHamburgerHoverOut),
+  navButton.addEventListener('click', e => {
+    navModal.classList.remove('hidden');
+  }),
+  navModal.addEventListener('click', () => {
+    navModal.classList.add('hidden');
+  });
